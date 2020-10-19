e820d6149b23cdce1f565735ca86c91349332e13
jihee <developer.jiheuilee@gmail.com>
2020-10-19 14:17:04 -0700

    commiting again

--- app/src/main/java/org/jiheuilee/notificationdemo/MainActivity.java
+++ app/src/main/java/org/jiheuilee/notificationdemo/MainActivity.java
@@ -1,14 +1,102 @@
 package org.jiheuilee.notificationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_notify;
+    private Button button_cancel;
+    private Button button_update;
+
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
+    //set the ID of the notification so that this can be updated or canceled later on
+    private static final int NOTIFICATION_ID = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+        button_notify = findViewById(R.id.bntNoti);
+        button_update = findViewById(R.id.btnUpdate);
+        button_cancel = findViewById(R.id.btnCancel);
+
+        button_notify.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                doNotify();
+            }
+        });
+
+        button_cancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                cancelNoti();
+            }
+        });
+        
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateNoti();
+            }
+        });
+    }
+
+    private void cancelNoti() {
+        mNotifyManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void updateNoti() {
+
+    }
+
+    private void doNotify(){
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+    public void createNotificationChannel(){
+
+        mNotifyManager =(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification",NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("you've been notified")
+                .setContentText("this is notification text")
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        return notifyBuilder;
     }
 }
